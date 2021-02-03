b543122d4d1ea5cc20e2299eeb675a27fe70e545
xwb <xwb0128@gmail.com>
2021-02-03 16:31:37 +0800

    misc:

--- src/main/resources/public/src/pages/index/App.vue
+++ src/main/resources/public/src/pages/index/App.vue
@@ -37,7 +37,7 @@ import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 
 import {Base64} from 'js-base64';
-import * as bytes from 'bytes';
+import * as pako from 'pako';
 
 import {ServerCommands} from "../../util/const.js"
 
@@ -350,30 +350,8 @@ class App extends Vue {
     }
   }
 
-  stringToByte(str) {  
-    var bytes = new Array();  
-    var len, c;  
-    len = str.length;  
-    for(var i = 0; i < len; i++) {  
-        c = str.charCodeAt(i);  
-        if(c >= 0x010000 && c <= 0x10FFFF) {  
-            bytes.push(((c >> 18) & 0x07) | 0xF0);  
-            bytes.push(((c >> 12) & 0x3F) | 0x80);  
-            bytes.push(((c >> 6) & 0x3F) | 0x80);  
-            bytes.push((c & 0x3F) | 0x80);  
-        } else if(c >= 0x000800 && c <= 0x00FFFF) {  
-            bytes.push(((c >> 12) & 0x0F) | 0xE0);  
-            bytes.push(((c >> 6) & 0x3F) | 0x80);  
-            bytes.push((c & 0x3F) | 0x80);  
-        } else if(c >= 0x000080 && c <= 0x0007FF) {  
-            bytes.push(((c >> 6) & 0x1F) | 0xC0);  
-            bytes.push((c & 0x3F) | 0x80);  
-        } else {  
-            bytes.push(c & 0xFF);  
-        }  
-    }  
-    return bytes;  
-  }  
+  
+  
 
   resetSheetRowAndCol(workbook){
     for(let i=0;i<workbook.getSheetCount();i++){
